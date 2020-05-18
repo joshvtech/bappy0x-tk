dafac2fc8f1f3c6f7bd9005a9d2082e2c7966192
Bappy0X <47988616+Bappy0X@users.noreply.github.com>
2020-05-18 13:33:29 +0100

    Make worker and key generation look a lil better

--- worker.py
+++ worker.py
@@ -1,11 +1,18 @@
 from uuid import uuid4
-from os import getenv
 
-token = str(uuid4())
+target = str(uuid4())
 with open(".env", "a") as file:
-    file.write(f"\nSECRET_API_TOKEN=\"{token}\"")
+    file.write(f"\nSECRET_API_TOKEN=\"{target}\"")
 
 from dotenv import load_dotenv
 load_dotenv(override=True)
+from os import getenv
 
-print(getenv("SECRET_API_TOKEN"))
+current = getenv("SECRET_API_TOKEN")
+
+if target == current:
+    token = current
+    print(f"SECRET_API_TOKEN was successfully set to \"{token}\".")
+else:
+    print(f"SECRET_API_TOKEN was unsuccessfully set. It is currently \"{current}\", when target was \"{target}\".\nShutting down...")
+    quit()
