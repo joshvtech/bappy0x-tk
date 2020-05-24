25e9f5bf6e3100570024698cfa2d65d21d1f172d
Bappy0X <47988616+Bappy0X@users.noreply.github.com>
2020-05-24 12:45:29 +0100

    Move over to GH pages

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: gunicorn "web:create_app()"
-worker: python worker.py
---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Coming soon
+
+$(document).ready(function() {
+    $("[data-toggle=\"tooltip\"]").tooltip();
+    $(".toast").toast("show");
+});
